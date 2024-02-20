f4ce2e7fc6fcd3e3c591a40a43f11f7518839851
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2024-02-20 15:20:35 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"products", component: ProductsComponent},
+  {path:"products/:category/:productId", component: ProductDetailComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"**",redirectTo:"",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
